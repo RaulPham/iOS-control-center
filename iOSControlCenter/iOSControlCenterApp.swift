7b09337f7169b7bcb8d1c5c38861893c039927d4
Raul Pham <phatpham314@icloud.com>
2023-04-21 10:00:57 -0500

    Initial Commit

---
+++ iOSControlCenter/iOSControlCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSControlCenterApp.swift
+//  iOSControlCenter
+//
+//  Created by Pham on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSControlCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
